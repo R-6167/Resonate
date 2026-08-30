c753d912785a015926f57e203665d27fc6270589
innotrepid <innotrepid@gmail.com>
2026-08-30 16:52:50 +0300

    Add CrossfadePresets class with duration presets

---
+++ lib/models/crossfade.dart
@@ -0,0 +1,11 @@
+class CrossfadePresets {
+  static const Map<String, double> presets = {
+    'Off': 0.0,
+    '0.5s': 500.0,
+    '1s': 1000.0,
+    '2s': 2000.0,
+    '3s': 3000.0,
+    '4s': 4000.0,
+    '5s': 5000.0,
+  };
+}
